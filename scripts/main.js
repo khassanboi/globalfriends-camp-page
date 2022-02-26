bc4e49ee74a6b68713184ca1a82cfd96074bf50c
khassanboi <khassanboi@gmail.com>
2022-02-26 14:06:04 +0500

    develop: creating eligibility and benefits sections

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,29 @@
-document.getElementById('menu-icon').addEventListener('click', () => {
-	document.getElementById('nav-list').classList.toggle('visible');
-});
+// document.getElementById('menu-icon').addEventListener('click', () => {
+// 	document.getElementById('nav-list').classList.toggle('visible');
+// });
 
-document.querySelectorAll(".nav__item-link").forEach(el => {
-	el.addEventListener('click',function (e) {
-		document.getElementById('nav-list').classList.toggle('visible');
-	});
+// document.querySelectorAll('.nav__item-link').forEach((el) => {
+// 	el.addEventListener('click', function (e) {
+// 		document.getElementById('nav-list').classList.toggle('visible');
+// 	});
+// });
+
+$('.owl-carousel').owlCarousel({
+	loop: true,
+	margin: 10,
+	nav: true,
+	autoplay: true,
+	autoplayTimeout: 3000,
+	autoplaySpeed: 1000,
+	responsive: {
+		0: {
+			items: 1,
+		},
+		600: {
+			items: 2,
+		},
+		1000: {
+			items: 3,
+		},
+	},
 });
